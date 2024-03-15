04b37591cfa02c129ded41ca5dfbc44f03e8550c
Vencoca <vaclav.kesler@gmail.com>
2024-03-15 23:00:28 +0100

    Hello route

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -8,6 +8,27 @@ if (!apiKey) {
   throw new Error("API_KEY environment variable is missing.");
 }
 
+export async function GET(req: NextRequest) {
+  const authorizationHeader = req.headers.get("Authorization");
+  if (!authorizationHeader) {
+    return NextResponse.json({
+      status: 401,
+      message: "Unauthorized: Missing Authorization header"
+    });
+  }
+
+  const incomingApiKey = authorizationHeader.replace("Bearer ", "");
+  if (incomingApiKey !== apiKey) {
+    return NextResponse.json({
+      status: 401,
+      message: "Unauthorized: Invalid API key"
+    });
+  }
+  return NextResponse.json({
+    status: 200
+  });
+}
+
 export async function POST(req: NextRequest) {
   const authorizationHeader = req.headers.get("Authorization");
   if (!authorizationHeader) {
